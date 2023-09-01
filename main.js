c812ed7a90587cc6dbed9c4d3ef11f8628340982
rixojustine <96961748+rixojustine@users.noreply.github.com>
2023-09-02 07:23:41 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('Hello World!');
+//javascript alert box
+
+alert("error9/15")
+alert("Your Device has been hacked :)")
+alert("your device will shutdown in 3 seconds")
+
+alert("3")
+alert("2")
+alert("1")
+prompt("nauto kano?? HAHAHAHA")
